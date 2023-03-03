aacf3bbac89a141a52df0d8d846dc29b768a1075
byungwook.ahn <byungwook.ahn@navercorp.com>
2023-03-03 17:27:36 +0900

    first commit

---
+++ src/test/kotlin/com/knoah/main/MainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knoah.main
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
